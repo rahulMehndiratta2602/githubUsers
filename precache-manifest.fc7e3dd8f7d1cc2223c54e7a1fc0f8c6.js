a22ec357ace1ad3fa99fb12f4e45780e8969fb62
rahulMehndiratta2602 <f20171479@hyderabad.bits-pilani.ac.in>
2021-10-20 15:50:24 +0530

    Updates

---
+++ precache-manifest.fc7e3dd8f7d1cc2223c54e7a1fc0f8c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225cabf9a8c7f4ee51709c2b8669d5e7",
+    "url": "/githubUsers/index.html"
+  },
+  {
+    "revision": "d1adeb4959c50d6f1bbb",
+    "url": "/githubUsers/static/css/main.ff45c5cb.chunk.css"
+  },
+  {
+    "revision": "4ef50281b7e3877516ff",
+    "url": "/githubUsers/static/js/2.78c45f95.chunk.js"
+  },
+  {
+    "revision": "d50cbe733d7e1e83d34da7158551fa9c",
+    "url": "/githubUsers/static/js/2.78c45f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1adeb4959c50d6f1bbb",
+    "url": "/githubUsers/static/js/main.f6e9474c.chunk.js"
+  },
+  {
+    "revision": "64ecafe4dcd521008e50",
+    "url": "/githubUsers/static/js/runtime-main.23bfae10.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/githubUsers/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/githubUsers/static/media/preloader.c114fda3.gif"
+  }
+]);
